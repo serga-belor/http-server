56cdb1d4a0306b85d38006dbe3868f0a35586e69
Sergey Belorusets <serga_belor_dev@outlook.com>
2018-09-23 19:25:16 +0300

    add 'About' page

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-var http = require("http");
+const http = require("http");
 
-http.createServer( function( req,res ) {
-    res.writeHead( 200, { "Content-Type": "text/plane"} );
-    res.end("Hello world!");
-} ).listen(33333);
+const port = 33333;
+const version = "0.0.0.1";
 
-console.log("Server is ruuning on 33333; press Ctrl+C to stop.");
+http.createServer( function( req, res ) {
+    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch( path ) {
+    case "":
+        res.writeHead( 200, { "Content-Type": "text/plane"} );
+        res.end("Sergey Belorusets's server.");
+        break;
+    case "/about":
+        res.writeHead( 200, { "Content-Type": "text/plane"} );
+        res.end(`Sergey Belorusets's server.\nVersion: ${version}`);
+        break;
+    default:
+        res.writeHead( 404, { "Content-Type": "text/plane"} );
+        res.end("Not found");
+        break;
+}
+} ).listen(port);
+
+console.log(`Server ver. ${version} is ruuning on ${port}; press Ctrl+C to stop.`);
